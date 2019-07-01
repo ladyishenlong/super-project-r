7f8fb839a26735c4240d93f1480a6d993de903d9
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2019-07-01 15:15:22 +0800

    oauth2 当前已经能获得授权码

---
+++ resource-oauth2-spr/src/main/java/com/ladyishenlong/resourceoauth2spr/ResourceOauth2SprApplication.java
@@ -0,0 +1,17 @@
+package com.ladyishenlong.resourceoauth2spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 参考 https://kefeng.wang/2018/04/06/oauth2-sso/
+ *
+ */
+@SpringBootApplication
+public class ResourceOauth2SprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceOauth2SprApplication.class, args);
+    }
+
+}
